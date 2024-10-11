3bacadbc01ddabd8cefe44fbcb56843c69e9510f
DiogoGuerra10 <d.guerra@ua.pt>
2024-10-11 16:14:40 +0100

    será desta lint

--- tdw-aleluia/eslint.config.js
+++ tdw-aleluia/eslint.config.js
@@ -1,14 +1,29 @@
 import js from '@eslint/js';
 import pluginReact from 'eslint-plugin-react';
+import babelParser from '@babel/eslint-parser';
 
-export default {
-  ...js.configs.recommended,
-  files: ['**/*.js', '**/*.jsx'], // Adapte conforme seu projeto
-  rules: {
-    'no-unused-vars': 'warn',
-    'react/react-in-jsx-scope': 'off', // Desativa a necessidade do import de React no JSX (React 17+)
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      parser: babelParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true, // Habilita a análise de JSX
+        },
+      },
+    },
+    files: ['**/*.js', '**/*.jsx'], // Adapte conforme seu projeto
+    rules: {
+      'no-unused-vars': 'warn',
+      'react/react-in-jsx-scope': 'off', // Desativa a necessidade do import de React no JSX (React 17+)
+    },
   },
-  plugins: {
-    react: pluginReact,
+  {
+    plugins: {
+      react: pluginReact,
+    },
   },
-};
+];
