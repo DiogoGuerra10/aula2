5369e177e64b4730c4644a4d103f1bda39d0e5d0
DiogoGuerra10 <d.guerra@ua.pt>
2024-10-11 16:06:51 +0100

    alteracoes

--- tdw-aleluia/eslint.config.js
+++ tdw-aleluia/eslint.config.js
@@ -1,18 +1,14 @@
 import js from '@eslint/js';
 import pluginReact from 'eslint-plugin-react';
 
-export default [
-  js.configs.recommended,
-  {
-    files: ['**/*.js', '**/*.jsx'], // Adapte conforme seu projeto
-    rules: {
-      'no-unused-vars': 'warn',
-      'react/react-in-jsx-scope': 'off', // Desativa a necessidade do import de React no JSX (React 17+)
-    },
+export default {
+  ...js.configs.recommended,
+  files: ['**/*.js', '**/*.jsx'], // Adapte conforme seu projeto
+  rules: {
+    'no-unused-vars': 'warn',
+    'react/react-in-jsx-scope': 'off', // Desativa a necessidade do import de React no JSX (React 17+)
   },
-  {
-    plugins: {
-      react: pluginReact,
-    },
+  plugins: {
+    react: pluginReact,
   },
-];
+};
