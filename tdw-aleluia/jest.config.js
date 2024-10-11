4e8009caa3d1d6e1618b53ac5fc792a2e67506d9
DiogoGuerra10 <d.guerra@ua.pt>
2024-10-11 17:15:49 +0100

    commit

---
+++ tdw-aleluia/jest.config.js
@@ -0,0 +1,16 @@
+/** @type {import('jest').Config} */
+const config = {
+    verbose: true,
+    testEnvironment: "jest-environment-jsdom",
+    testMatch: [
+      "**/__tests__/**/*.[jt]s?(x)", // Corrigido
+      "**/?(*.)+(spec|test).[tj]s?(x)", // Corrigido
+    ],
+    collectCoverage: true,
+    testPathIgnorePatterns: ["<rootDir>/node_modules/"], // Usar <rootDir> para melhor compatibilidade
+    transform: {
+      "^.+\\.[t|j]sx?$": "babel-jest",
+    },
+  };
+  
+  module.exports = config;
